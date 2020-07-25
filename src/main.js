371e935850b9e91ffa57af4fb9253aefd1adee6d
lilinxi015 <63709448+lilinxi015@users.noreply.github.com>
2020-07-25 23:09:40 +0800

    v1.0

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+
+Vue.use(ViewUI);
+
 Vue.config.productionTip = false
+Vue.prototype.$bus=new Vue()
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,100 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+const DiscoverMusic=()=>import('views/discover/DiscoverMusic')
+const Individuation=()=>import('views/discover/childRouter/Individuation')
+const MusicListCategory=()=>import('views/discover/childRouter/MusicListCategory')
+const MusicRankList=()=>import('views/discover/childRouter/MusicRankList')
+const ArtistCategory=()=>import('views/discover/childRouter/ArtistCategory')
+const NewSongsR=()=>import('views/discover/childRouter/NewSongsR')
+
+const MusicListDetail=()=>import('views/musicListDetail/MusicListDetail')
+const DayMusicListDetail=()=>import('views/musicListDetail/DayMusicListDetail')
+
+const ArtistDetail=()=>import('views/artistdetail/ArtistDetail')
+const ArtistAlbum=()=>import('views/artistdetail/childRouter/ArtistAlbum')
+const ArtistDescDetail=()=>import('views/artistdetail/childRouter/ArtistDescDetail')
+const SimiArtist=()=>import('views/artistdetail/childRouter/SimiArtist')
+
+const SearchList=()=>import('views/search/SearchList')
 
 Vue.use(VueRouter)
 
   const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+      path:'',
+      redirect:'/discover'
+    },
+    {
+      path:'/discover',
+      component:DiscoverMusic,
+      children:[
+        {
+          path:'/',
+          redirect:'/discover/individ'
+        },
+        {
+          path:'/discover/individ',
+          component:Individuation
+        },
+        {
+          path:'/discover/category',
+          component:MusicListCategory
+        },
+        {
+          path:'/discover/ranklist',
+          component:MusicRankList
+        },
+        {
+          path:'/discover/artist',
+          component:ArtistCategory
+        },
+        {
+          path:'/discover/newsongs',
+          component:NewSongsR
+        }
+      ]
+    },
+    {
+      path:'/musiclistdetail/:id/:time',
+      name:'detail',
+      component:MusicListDetail,
+    },
+    {
+      path:'/daymusic',
+      component:DayMusicListDetail
+    },
+    {
+      path:'/artist',
+      component:ArtistDetail,
+      children:[
+        {
+          path:'/',
+          redirect:'/artist/album'
+        },
+        {
+          path:'/artist/album',
+          component:ArtistAlbum
+        },
+        {
+          path:'/artist/desc',
+          component:ArtistDescDetail
+        },
+        {
+          path:'/artist/simi',
+          component:SimiArtist
+        },
+      ]
+    },
+    {
+      path:'/search/:key',
+      component:SearchList
+    }
 ]
 
 const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
 
